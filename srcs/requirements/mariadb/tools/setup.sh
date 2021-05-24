f62f3a4673f8c7e10f80d8d8626d4d7c34d1db92
jaekpark <jaekpark@student.42seoul.kr>
2021-05-25 07:48:16 +0900

    fix setup.sh & bind option of 50-server.cnf file.

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -5,16 +5,11 @@ cat << EOF > setup.sql
 USE mysql;
 FLUSH PRIVILEGES;
 
-# init root user password && create an root user for external host and grant authority.
-
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
-
 # create database for wp service.
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
 
-# Create wp users and grant authority.
 CREATE USER IF NOT EXISTS '$MYSQL_ADMIN'@'%' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD';
-GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_ADMIN'@'%' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_ADMIN'@'%';
 
 # Apply Changes
 FLUSH PRIVILEGES;
--- srcs/requirements/mariadb/tools/setup.sql
+++ srcs/requirements/mariadb/tools/setup.sql
@@ -2,16 +2,11 @@
 USE mysql;
 FLUSH PRIVILEGES;
 
-# init root user password && create an root user for external host and grant authority.
-
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '' WITH GRANT OPTION;
-
 # create database for wp service.
 CREATE DATABASE IF NOT EXISTS ;
 
-# Create wp users and grant authority.
 CREATE USER IF NOT EXISTS ''@'%' IDENTIFIED BY '';
-GRANT ALL PRIVILEGES ON .* TO ''@'%' IDENTIFIED BY '' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO ''@'%';
 
 # Apply Changes
 FLUSH PRIVILEGES;
