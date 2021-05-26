2bedb254564418aadf1b7f6427cf2dc55ae85312
jaekpark <jaekpark@student.42seoul.kr>
2021-05-26 20:12:19 +0900

    Fix setup.sh

--- srcs/requirements/tools/setup.sh
+++ srcs/requirements/tools/setup.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
 
 ID=jaekpark.42.fr
+CNT=$(cat /etc/hosts | grep "127.0.0.1 ${ID}" | wc -l)
 
 append()
 {
-	if [[ $(cat /etc/hosts | grep "127.0.0.1 ${ID}" | wc -l) -eq 0 ]] ; then
+	if [[ $(CNT) -eq 0 ]] ; then
 		echo "127.0.0.1 ${ID}" | sudo tee -a /etc/hosts
 	fi
 }
 
 delete()
 {
-	if [[ $(cat /etc/hosts | grep "127.0.0.1 ${ID}" | wc -l) -gt 0 ]] ; then
+	if [[ $(CNT) -gt 0 ]] ; then
 		sudo sed -i "/127.0.0.1 ${ID}/d" /etc/hosts
 	fi
 }
