0fbe355d3dc30b3cdf77fab6d0fa8ba37f40b946
jaekpark <jaekpark@student.42seoul.kr>
2021-05-29 20:27:17 +0900

    fix setup.sh for wordpress

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-wp core install --admin_user=$MYSQL_ADMIN --admin-password=$MYSQL_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --url=https://$WORDPRESS_DOMAIN --title='INCEPTION
+wp core install --path=/var/www/wordpress --admin_user=$MYSQL_ADMIN \
+	--admin-password=$MYSQL_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL \
+	--url=https://$WORDPRESS_DOMAIN --title='INCEPTION'
 
