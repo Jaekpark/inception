25ed758e71f8734df0545a3a132e64f640eeda17
jaekpark <jaekpark@student.42seoul.kr>
2021-05-29 20:00:55 +0900

    fix wordpress config : wordpress admin config using wp-cli

---
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+wp core install --admin_user=$MYSQL_ADMIN --admin-password=$MYSQL_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --url=https://$WORDPRESS_DOMAIN --title='INCEPTION
+
