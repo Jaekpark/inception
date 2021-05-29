8ea57b639418426f313315f7a6320e8ff2afc573
jaekpark <jaekpark@student.42seoul.kr>
2021-05-29 19:07:46 +0900

    fix mariadb entrypoin.sh

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 ./setup.sh
+service mysql start
 mysql -u root -password=$MYSQL_ROOT_PASSWORD < setup.sql
 /usr/bin/mysqld_safe --user=root --datadir=/var/lib/mysql/ --general-log-file=/dev/stderr
