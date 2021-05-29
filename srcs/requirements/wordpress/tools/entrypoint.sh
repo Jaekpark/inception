3dde8b109fc91e4a3cb140b96d26eb14d4434276
jaekpark <jaekpark@student.42seoul.kr>
2021-05-29 21:41:07 +0900

    fix wordpress

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,7 +1,4 @@
 #! /bin/sh
-
-wp core install --path=/var/www/wordpress --admin_user=${MYSQL_ADMIN} \
-	--admin_password=${MYSQL_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL} \
-	--url=${WORDPRESS_DOMAIN} --title=INCEPTION --allow-root
-
+./setup.sh
+rm setup.sh
 php-fpm7.3 --nodaemonize
