bdb557943467ba2c0cef681ef934d2becb698013
jaekpark <jaekpark@student.42seoul.kr>
2021-05-29 20:37:47 +0900

    fix wordpress dockerfile and entrypoint.sh

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -4,3 +4,4 @@ wp core install --path=/var/www/wordpress --admin_user=$MYSQL_ADMIN \
 	--admin-password=$MYSQL_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL \
 	--url=https://$WORDPRESS_DOMAIN --title='INCEPTION' --allow-root
 
+php-fpm7.3 --nodaemonize
