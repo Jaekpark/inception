846e8d0000cd204dd9c5b6537e00e410f72eea6f
jaekpark <jaekpark@student.42seoul.kr>
2021-05-29 22:16:02 +0900

    test

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,6 +1,4 @@
-#! /bin/bash
+#! /bin/sh
 
-./setup.sh
-service mysql start
-mysql -u root --password=$MYSQL_ROOT_PASSWORD < setup.sql
+./setup.sh && service mysql start && mysql -u root --password=$MYSQL_ROOT_PASSWORD < setup.sql
 /usr/bin/mysqld_safe --user=root --datadir=/var/lib/mysql/ --general-log-file=/dev/stderr
