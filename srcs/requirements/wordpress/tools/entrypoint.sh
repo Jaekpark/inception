fe62fa030923c1a61990966bef3b9970c211f6a4
jaekpark <jaekpark@student.42seoul.kr>
2021-05-29 20:41:19 +0900

    fix yml file and env

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 wp core install --path=/var/www/wordpress --admin_user=$MYSQL_ADMIN \
-	--admin-password=$MYSQL_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL \
+	--admin_password=$MYSQL_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL \
 	--url=https://$WORDPRESS_DOMAIN --title='INCEPTION' --allow-root
 
 php-fpm7.3 --nodaemonize
